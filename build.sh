ed9b4171b313e3a0dcd7ccafd259513dba40adf9
Matthew Pontefract <matthew@rethought-solutions.com>
2012-08-11 15:50:40 +0100

    wip

--- build.sh
+++ build.sh
@@ -38,5 +38,8 @@ pep8 --ignore=W293,E128,E501,E127 --exclude=migrations,manage.py,docs,assets,set
 # due to MP inability to get Jenkins to find the files here, let's move them
 # up one
 echo We are currently in: `pwd`
+echo Build ID: ${BUILD_ID}
+echo Build No: ${BUILD_NUMBER}
+ls ../../builds
 mv pep8.txt ..
 mv ../../builds/${BUILD_ID}/coverage.xml ..
